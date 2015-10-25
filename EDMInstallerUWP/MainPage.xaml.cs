d1d94e1ebaf86623f37223ef2e6b7a5796dec46e
Zach Dowding <zdowding@gmail.com>
2015-10-25 14:04:38 -0600

    Main Buttons
    +added Column defintions and Row Definitions
    +added file dialog

--- EDMInstallerUWP/MainPage.xaml.cs
+++ EDMInstallerUWP/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,46 @@ namespace EDMInstallerUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static MainPage Current;
+
         public MainPage()
         {
             this.InitializeComponent();
+            PickFolderButton.Click += new RoutedEventHandler(PickFolderButton_Click);
+            Current = this;
+        }
+
+        private void Install_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void Find_Directory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void PickFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear previous returned folder name, if it exists, between iterations of this scenario
+            OutputTextBlock.Text = "";
+
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".docx");
+            folderPicker.FileTypeFilter.Add(".xlsx");
+            folderPicker.FileTypeFilter.Add(".pptx");
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder (including other sub-folder contents)
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                OutputTextBlock.Text = "Picked folder: " + folder.Name;
+            }
+            else
+            {
+                OutputTextBlock.Text = "Operation cancelled.";
+            }
         }
     }
 }
